f0bda60e2ad5b1c79674bdd0fbee8279beae2973
Daniel Rice <Daniel.L.Rice@gmail.com>
2015-05-31 23:56:16 +0100

    Adding dalliance holder with styles

--- src/app/components/qc.jsx
+++ src/app/components/qc.jsx
@@ -17,28 +17,93 @@ var QC = React.createClass({
   
   render() {
     return (
-      <QCToolbar />
+      <div>
+        <QCToolbar />
+        <DallianceHolder />
+      </div>
     );
   }
 
 });
 
 
+var DallianceHolder = React.createClass({
+  componentDidMount() {
+    var b = new Browser({
+      chr:          '18',
+      viewStart:    117141,
+      viewEnd:      117341,
+      // chr:          '16',
+      // viewStart:    48000629,
+      // viewEnd:      48000820,
+      // noPersistView : true,
+      cookieKey:    'human-grc_h37',
+      coordSystem: {
+        speciesName: 'Human',
+        taxon: 9606,
+        auth: 'GRCh',
+        version: '37',
+        ucscName: 'hg19'
+      },
+      maxHeight : 10000,
+      setDocumentTitle: true,
+      //uiPrefix: window.location.origin + '/',
+      fullScreen: true,
+      disableDefaultFeaturePopup : true,
+      noPersist : true,
+      maxWorkers : 3,
+      baseColors: {
+        A: 'green', 
+        C: 'blue', 
+        G: 'orange', 
+        T: 'red',
+        '-' : 'black', // deletion
+        I : 'mediumpurple' // insertion
+      },
+      sources: [
+        {
+          name: 'Genome',
+          twoBitURI: 'http://www.biodalliance.org/datasets/hg19.2bit',
+          tier_type: 'sequence',
+          provides_entrypoints: true,
+          pinned: true
+        }
+      ]
+    });
+  },
+
+  render() {
+    var style = {
+      backgroundColor: 'white',
+      border: '4px solid black'
+    }
+    return (
+        <div id="svgHolder" style={style}></div>
+    );
+  }
+
+});
+
 var QCToolbar = React.createClass({
 
   render() {
+
       return (
-        <Navbar brand='Snoopy' inverse toggleNavKey={0}>
-          <Nav right eventKey={0}> {/* This is the eventKey referenced */}
-            <NavItem eventKey={1} href='#'>Settings</NavItem>
-            <NavItem eventKey={2} href='#'>Help</NavItem>
-            <NavItem eventKey={2} href='#'>About</NavItem>
-            <NavItem eventKey={2} href='#'>GitHub</NavItem>
+        <Navbar toggleNavKey={0} className='QCToolbar'>
+          <Nav eventKey={0}>
+            <NavItem eventKey={2} href='#'><Button>chr16:233423</Button></NavItem>
+            <NavItem eventKey={1} href='#'><Button><Glyphicon glyph='home' /></Button></NavItem>
+            <NavItem eventKey={2} href='#'><Button><Glyphicon glyph="chevron-left"/>Previous</Button></NavItem>
+            <NavItem eventKey={3} href='#'><Button>Not a Variant<Glyphicon glyph="chevron-right"/></Button></NavItem>
+            <NavItem eventKey={4} href='#'><Button>Uncertain<Glyphicon glyph="chevron-right"/></Button></NavItem>
+            <NavItem eventKey={5} href='#'><Button>Variant<Glyphicon glyph="chevron-right"/></Button></NavItem>
           </Nav>
-        </Navbar>      
+        </Navbar>   
       );    
   }
-
+                    // <button type="button" class="btn btn-default qc-decision" id="buttonQCNotVariant" data-value="not variant">Not a Variant <span class="glyphicon glyphicon-chevron-right"></span></button>
+                    // <button type="button" class="btn btn-default qc-decision" id="buttonQCPotentialVariant" data-value="uncertain">Uncertain <span class="glyphicon glyphicon-chevron-right"></span></button>
+                    // <button type="button" class="btn btn-default qc-decision" id="buttonQCCertainVariant"  data-value="variant">Variant <span class="glyphicon glyphicon-chevron-right"></span></button>
 });
 
 module.exports = QC;
