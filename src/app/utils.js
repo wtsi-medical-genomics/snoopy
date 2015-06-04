8d4a29c1147b02315eadc87ab4e0fff3d9353fc7
Daniel Rice <Daniel.L.Rice@gmail.com>
2015-06-04 10:22:27 +0100

    Adding combineServerPath to utils.js

--- src/app/utils.js
+++ src/app/utils.js
@@ -1,9 +1,12 @@
 "use strict;"
 
+var RE_EXT = /^.*\.(.*)$/;
+var RE_LEFT_SLASH = /\/(.*)/;
+var RE_RIGHT_SLASH = /(.*)\//;
+
 function getExtension(f) {
     f = typeof(f) === 'string' ? f : f.name;
-    var re_ext = /^.*\.(.*)$/;
-    var m = f.match(re_ext);
+    var m = f.match(RE_EXT);
     return m ? m[1].toLowerCase() : null;
 }
 
@@ -12,6 +15,25 @@ function getName(f) {
       f = f.name;
   var parts = f.split('/');
   return parts[parts.length - 1];
+}
+
+function arrayStringContains(el, arr) {
+  console.log(el);
+  console.log(typeof(el));
+  if (typeof(el) === 'undefined' || el === null || el.length === 0)
+    return false;
+  el = el.toLowerCase();
+  for (var i=0; i<arr.length; ++i) {
+    console.log(i)
+    if (el === arr[i].toLowerCase())
+      return true;
+  }
+}
+
+function combineServerPath(server, path) {
+  server = server.match(RE_LEFT_SLASH)[1];
+  path = path.match(RE_RIGHT_SLASH)[1];
+  return server + '/' + path;
 }
 
 // class UID {
@@ -39,5 +61,6 @@ UID.prototype.next = function() {
 module.exports = {
   getName: getName,
   getExtension: getExtension,
-  UID: UID
+  UID: UID,
+  arrayStringContains: arrayStringContains
 }
